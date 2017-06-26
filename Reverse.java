6a3946c24438672c96846ea2a948713a676c0402
DEEBIKA1305 <tdeepika4eee@gmail.com>
2017-06-26 15:59:02 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,16 @@
+package set1;
+import java.util.*;
+public class Reverse {
+public static void main(String args[]){
+	Scanner s=new Scanner(System.in);
+	String str;
+	str=s.next();
+	StringBuffer s1=new StringBuffer(str);
+	System.out.println(s1.reverse());
+	
+	
+	
+	
+	
+}
+}
