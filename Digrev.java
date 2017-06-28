30b142982d14d3eb529fd6dd50e36d742b19fe8b
DEEBIKA1305 <tdeepika4eee@gmail.com>
2017-06-28 15:39:44 +0530

    Add files via upload

---
+++ Digrev.java
@@ -0,0 +1,21 @@
+package numberbased;
+
+import java.util.*;
+public class Digrev {
+	public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+System.out.println("Enter any number:");
+int num=s.nextInt();
+
+int n,rev=0,i=0;
+while(num>0){
+	n=num%10;
+	
+	rev=rev*10+n;
+	i++;
+	num=num/10;
+}
+System.out.println(rev);
+}
+}
+
